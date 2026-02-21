82502df0f41016fff6e9a4e2b65565bc5e558a57
Vishnu <vishnusvprasad@gmail.com>
2026-02-21 13:12:07 +0530

    updated cors isssue

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://assignment-14-crm-9m7t.vercel.app/"], 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.json({ msg: "Home page crms" });
